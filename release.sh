c72c5bcd4000ac8e74a21dac548d9c2b38d99b0f
Philipp Keck <git@philippkeck.de>
2021-09-04 21:09:49 +0200

    Chrome app release process

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+release() {
+    if ! [[ -f "dist/manifest.json" ]]; then
+        echo "Please build the extension first with build.sh"
+        exit 1
+    fi
+    rm -f release.zip
+    cd dist || return
+    zip -r ../release.zip ./*
+    echo "Please upload release.zip to the Chrome store: "
+}
+
+release
