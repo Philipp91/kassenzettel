41df01197cfdb4d52dc6e3602d9d7e44aac30638
Philipp Keck <git@philippkeck.de>
2022-01-30 22:14:26 +0100

    Minor fixes

--- public/background.js
+++ public/background.js
@@ -2,6 +2,7 @@ let lastData = null;
 chrome.runtime.onMessage.addListener(({csvDatas, getCsv}, sender, sendResponse) => {
     if (Array.isArray(csvDatas)) {
         lastData = csvDatas;
+        // noinspection JSIgnoredPromiseFromCall
         chrome.tabs.create({url: 'index.html'});
     } else if (getCsv) {
         sendResponse({csvDatas: lastData});
